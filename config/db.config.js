71cdd828d280d8708536a5505061a76199a40f2f
gimmickygyudon <gimmickygyudon@gmail.com>
2023-06-06 07:20:45 +0700

    local

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
 
+const db_local = new Sequelize("indostar", "root", "", {
+    host: "localhost",
+    dialect: "mysql",
+    timezone: "+07:00",
+})
+
 const db = new Sequelize("mis", "mysql_client", "1BMclient!", {
     host: "192.168.1.8",
     dialect: "mysql",
     timezone: "+07:00",
 });
 
-export default db;
+export default db_local;
